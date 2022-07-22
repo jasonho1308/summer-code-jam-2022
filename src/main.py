874c46e2ce5f6e1ba07c91d2387d9d3c038f67bf
aphkyle <81857274+aphkyle@users.noreply.github.com>
2022-07-23 00:05:29 +0800

    [+] Docstrings

--- src/main.py
+++ src/main.py
@@ -27,20 +27,62 @@ html = """
 
 
 class ConnectionManager:
+    """
+    Managing connection between client and server.
+
+    Attributes
+    ----------
+    active_connections: List[WebSocket]
+        Current alive connections that is connected to WebSocket
+
+    Methods
+    -------
+    __init__()
+        Initalize the manager
+        Set `active_connections`
+    connect(websocket: WebSocket)
+        Called when client connects to WebSocket
+        Assign connection to `active_connections`
+    disconnect(websocket: WebSocket)
+        Called when client disconnects
+        Remove connection from `active_connections`
+    send_to_client(message: str, websocket: WebSocket)
+        Sends message to the specified client
+    broadcast(message: str)
+        Sends message to all active connection
+    """
     def __init__(self):
+        """__init__()
+        Initalize the manager
+        Set `active_connections`
+        """
         self.active_connections: List[WebSocket] = []
 
     async def connect(self, websocket: WebSocket):
+        """connect(websocket: WebSocket)
+        Called when client connects to WebSocket
+        Assign connection to `active_connections`
+        """
         await websocket.accept()
         self.active_connections.append(websocket)
 
     def disconnect(self, websocket: WebSocket):
+        """disconnect(websocket: WebSocket)
+        Called when client disconnects
+        Remove connection from `active_connections`
+        """
         self.active_connections.remove(websocket)
 
     async def send_to_client(self, message: str, websocket: WebSocket):
+        """send_to_client(message: str, websocket: WebSocket)
+        Sends message to the specified client
+        """
         await websocket.send_text(message)
 
     async def broadcast(self, message: str):
+        """broadcast(message: str)
+        Sends message to all active connection
+        """
         for connection in self.active_connections:
             await connection.send_text(message)
 
@@ -50,11 +92,17 @@ manager = ConnectionManager()
 
 @app.get("/")
 async def get():
+    """get()
+    Called when root of site accessed
+    """
     return HTMLResponse(html)
 
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    """websocket_endpoint(websocket: WebSocket, client_id: str)
+    Starts WebSocket when /ws/{some random UUID} accessed
+    """
     await manager.connect(websocket)
     try:
         while True:
