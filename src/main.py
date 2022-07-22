80b3854397a67367b12b8f5786a37a037b633937
aphkyle <81857274+aphkyle@users.noreply.github.com>
2022-07-22 23:46:45 +0800

    [+] Basic socket functions
    [server]
    [+] multi clients
    [+] send_to_client
    [+] broadcast
    [client]
    [+] unique ids
    [+] send/receive
    [+] you can chat now in console, with methods in WebSocket (javascript)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, WebSocket
+from typing import List
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -7,30 +9,57 @@ html = """
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Burly Barghests</title>
+        <title>Chat</title>
     </head>
     <body>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <script>
+            var client_id = crypto.randomUUID()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://`+ window.location.host + `/ws/${client_id}`);
+            ws.addEventListener('message', function (event) {
+                console.log('From server:', event.data);
+            });
+        </script>
     </body>
 </html>
 """
 
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_to_client(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
 @app.get("/")
 async def get():
-    """get()
-
-    Get request event on root of site.
-    """
     return HTMLResponse(html)
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    """websockets_endpoint(websockets: WebSocket)
-
-    WebSocket start
-    """
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_to_client(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
