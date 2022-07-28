0a1ce58b77e6d9203039e7da22c8be67b00c25a3
aphkyle <81857274+aphkyle@users.noreply.github.com>
2022-07-28 18:05:55 +0800

    [fix] NoSuchModuleError
    NoSuchModuleError: Can't load plugin: sqlalchemy.dialects:postgres

--- src/database.py
+++ src/database.py
@@ -4,7 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
+SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"].replace("postgres://", "postgresql://")
+# SQLAlchemy deprecated `postgres://` had to use the latter one now
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
