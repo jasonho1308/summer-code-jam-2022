0f2bea23f73300059bb63da630d8821707745d2f
aphkyle <81857274+aphkyle@users.noreply.github.com>
2022-07-28 18:07:05 +0800

    [fix] lint

--- src/ActionManager.py
+++ src/ActionManager.py
@@ -24,9 +24,9 @@ class ActionManager:
         hashed = db.execute(
             select(models.Player.hashed_password).where(models.Player.name == data["name"])
         )
-        db.close() # close the conn asap
+        db.close()  # close the conn asap
         if bcrypt.checkpw(data["password"], hashed):
             self.certificated.append(client_id)
-            connection_manager.send_to_client(f"Welcome!")
+            connection_manager.send_to_client("Welcome!")
         else:
             connection_manager.send_to_client("Login failed, incorrect username or password")
