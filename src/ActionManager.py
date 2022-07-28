9d1d23116e494c8ea3813b5d7ac2d8b1f4247ad4
aphkyle <81857274+aphkyle@users.noreply.github.com>
2022-07-28 17:28:09 +0800

    [fix] lint

--- src/ActionManager.py
+++ src/ActionManager.py
@@ -5,13 +5,26 @@ from . import database, models
 
 
 class ActionManager:
+    """Handling actions from client"""
+
     certificated = []
 
     def login(self, data, client_id):
+        """
+        Handles login request
+
+        JSON Structure:
+        {
+            "action": "login"
+            "name": "xxx"
+            "password": "xxx"
+        }
+        """
+
         db = database.SessionLocal()
         hashed = db.execute(
-            select(models.Player.hashed_password).where(Player.name == data["name"])
+            select(models.Player.hashed_password).where(models.Player.name == data["name"])
         )
-        if bcrypt.checkpw(passwd, hashed):
+        if bcrypt.checkpw(data["password"], hashed):
             self.certificated.append(client_id)
         db.close()
