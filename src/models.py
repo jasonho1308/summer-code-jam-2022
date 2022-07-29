afaa065c48a73b4b519f6afe18a2d45905f03c93
David Yang <davidyang6us@gmail.com>
2022-07-29 15:55:50 +0800

    Further database schema adjustments

--- src/models.py
+++ src/models.py
@@ -9,8 +9,8 @@ class Player(Base):
 
     __tablename__ = "players"
 
-    UUID = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(32), index=True)
+    UUID = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    name = Column(String(32), unique=True, index=True)
     hashed_password = Column(String(72))
     role = Column(String, index=True)
     hp = Column(Integer)
@@ -21,15 +21,13 @@ class Player(Base):
     pve_cooldown = Column(TIMESTAMP)
     pvp_cooldown = Column(TIMESTAMP)
 
-    items = relationship("Item", back_populates="owner")
-
 
 class Item(Base):
     """Item in ORM"""
 
     __tablename__ = "items"
 
-    item_id = Column(Integer, primary_key=True, autoincrement=True)
+    item_id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
     title = Column(String, index=True)
     item_type = Column(String)
     equipped = Column(Boolean)
@@ -37,7 +35,7 @@ class Item(Base):
     item_info = Column(JSON)
     owner = Column(Integer, ForeignKey("players.UUID"), index=True)
 
-    owner = relationship("User", back_populates="items")
+    players = relationship("Player")
 
 
 Base.metadata.create_all(engine)
