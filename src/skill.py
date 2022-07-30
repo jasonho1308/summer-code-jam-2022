e5693559253a21e16a3a9d088cf3c59a083a990a
aphkyle <81857274+aphkyle@users.noreply.github.com>
2022-07-30 18:02:32 +0800

    [+] `use`

--- src/skill.py
+++ src/skill.py
@@ -15,6 +15,19 @@ class Skill:
     dexterity = None
     charisma = None
 
-    def use(user, opponent):
+    def _use(self, user, opponent):
         """Implement effects on the user and opponent here"""
         pass
+
+    def use(self, user, opponent):
+        """Decorator for _use, used for checking if skill usable"""
+        if user.hp <= 0 and opponent.hp <= 0:
+            return (user, opponent, "You both are dead, no rewards")
+        if user.hp <= 0:
+            return (user, opponent, f"{user.name} is dead, {opponent.name} wins")
+        elif opponent.hp <= 0:
+            return (user, opponent, f"{opponent.name} is dead, {user.name} wins")
+        if user.energy < energy_cost:
+            return (user, opponent, f"{user.name} don't have enough energy to cast {self.name}!")
+        user.energy -= energy_cost
+        return  _use(user, opponent)
