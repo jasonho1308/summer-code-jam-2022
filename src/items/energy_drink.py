b3c291b6c66ddb7e054400dc45232ad3a751b7dc
kyky <81857274+aphkyle@users.noreply.github.com>
2022-07-31 23:18:07 +0800

    add back staticmethod

--- src/items/energy_drink.py
+++ src/items/energy_drink.py
@@ -7,6 +7,7 @@ class EnergyDrink(Item):
     item_type_id = 2
     name = "energy_drink"
 
+    @staticmethod
     def use(player):
         """Refill all energy"""
         player.energy = player.max_energy
