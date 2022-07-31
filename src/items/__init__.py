7e37cf1bb7bf27b0b0556456fac6c583eae0aaf6
kyky <81857274+aphkyle@users.noreply.github.com>
2022-07-31 23:27:40 +0800

    [structure]

--- src/items/__init__.py
+++ src/items/__init__.py
@@ -1,12 +1,12 @@
 from . import energy_drink, health_potion
 
-items = [
-    {
-        "energy_drink": energy_drink.EnergyDrink,
-        "cost": 25,
-    },
-    {
-        "health_potion": health_potion.HealthPotion,
-        "cost": 25,
-    }
-]
+items = {
+    "energy_drink": [
+        energy_drink.EnergyDrink,
+        25,
+    ],
+    "health_potion": [
+        health_potion.HealthPotion,
+        25,
+    ]
+}
