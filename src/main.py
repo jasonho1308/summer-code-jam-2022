59c2e5aa22fcd39d89e7cadfa415662aec72c1f4
David Yang <davidyang6us@gmail.com>
2022-08-01 01:01:43 +0800

    Cut extraneous tracebacks
    only there for my quick debugging

--- src/main.py
+++ src/main.py
@@ -41,15 +41,9 @@ async def websocket_endpoint(websocket: WebSocket):
                 await connection_manager.send_to_client(
                     "Invalid action specified", websocket
                 )
-                await connection_manager.send_to_client(
-                    traceback.format_exc(), websocket
-                )
             except KeyError as e:
                 await connection_manager.send_to_client(
                     f"Invalid data, did you mean {e}?", websocket
-                )
-                await connection_manager.send_to_client(
-                    traceback.format_exc(), websocket
                 )
             except Exception:
                 await connection_manager.send_to_client(
