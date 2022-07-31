1ec8871f37bb507e621f9f2a9f72e16bec1b5609
kyky <81857274+aphkyle@users.noreply.github.com>
2022-07-31 23:19:10 +0800

    Update __init__.py

--- src/items/__init__.py
+++ src/items/__init__.py
@@ -1,1 +1,12 @@
-# flake8: noqa
+from . import energy_drink, health_potion
+
+items = [
+    {
+        "energy_drink": energy_drink.EnergyDrink,
+        "cost": 25,
+    },
+    {
+        "health_potion": health_potion.HealthPotion,
+        "cost": 25,
+    }
+]
