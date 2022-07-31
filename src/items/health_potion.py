cba532d0c2d8202ceea2b0803983dbbf9992f951
kyky <81857274+aphkyle@users.noreply.github.com>
2022-07-31 23:14:08 +0800

    fix typo

--- src/items/health_potion.py
+++ src/items/health_potion.py
@@ -9,7 +9,7 @@ class HealthPotion(Item):
 
     def use(player):
         """Heals player for 50 HP"""
-        if (player.hp + 50) > player.maxhp:
-            player.hp = player.maxhp
+        if (player.hp + 50) > player.max_hp:
+            player.hp = player.max_hp
         else:
             player.hp += 50
