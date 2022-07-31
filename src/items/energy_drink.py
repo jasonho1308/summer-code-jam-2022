1a88e6606583c46ecef24ab7be2a3d1e80a767d8
kyky <81857274+aphkyle@users.noreply.github.com>
2022-07-31 23:15:33 +0800

    Create energy_drink.py

---
+++ src/items/energy_drink.py
@@ -0,0 +1,12 @@
+from .item import Item
+
+
+class EnergyDrink(Item):
+    """Refill all energy"""
+
+    item_type_id = 2
+    name = "energy_drink"
+
+    def use(player):
+        """Refill all energy"""
+        player.energy = player.max_energy
