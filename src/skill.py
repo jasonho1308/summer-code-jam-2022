46f71d73eb8bce50bfbbe4213b95ab47d94127fb
kyky <81857274+aphkyle@users.noreply.github.com>
2022-07-31 20:14:38 +0800

    + func to check if learnt

--- src/skill.py
+++ src/skill.py
@@ -1,13 +1,9 @@
-import random
-
-
 class Skill:
     """Template class for skills"""
 
     name: str
     description: str
     energy_cost: int
-    chance: float
 
     # Requirements for players if we get around to players learning skills
     level: int
@@ -17,27 +13,31 @@ class Skill:
     dexterity: int
     charisma: int
 
-    @property
-    def name(self):
-        """The name of the skill"""
-        return self.__class__.__name__
+    @classmethod
+    def _use(cls, user, opponent):
+        """Implement effects on the user and opponent here"""
+        pass
 
-    def _use(self, user, opponent):
-        """Skill implementation"""
-        if random.random() < self.chance:
-            damage = int(user.strength * (random.random() + 1))
-            opponent.hp -= damage
-            return f"{user.name!r} uses {self.name!r} for {damage} damage"
-        else:
-            return f"{user.name!r} tried to use {self.name!r} but missed!"
-
-    def use(self, user, opponent):
+    @classmethod
+    def use(cls, user, opponent):
         """Decorator for _use, used for checking if skill usable"""
         if user.energy < self.energy_cost:
             return (
                 user,
                 opponent,
-                f"{user.name!r} doesn't have enough energy to cast {self.name!r}!",
+                f"{user.name} doesn't have enough energy to cast {self.name}!",
             )
         user.energy -= self.energy_cost
-        return self._use(user, opponent)
+        return cls._use(user, opponent)
+
+    @classmethod
+    def learnt(cls, player):
+        return (
+            user.level >= self.level
+            and user.strength >= self.strength
+            and user.intellegence >= self.intelligence
+            and user.stamina >= self.stamina
+            and user.dexterity >= self.dexterity
+            and user.charisma >= self.charisma
+            and user.gold >= self.gold
+        )
