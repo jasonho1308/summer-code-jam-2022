21cf9ceb9938f82e3a274ecba5dfccfbd0165300
kyky <81857274+aphkyle@users.noreply.github.com>
2022-07-31 19:46:08 +0800

    [-] return values

--- src/fight.py
+++ src/fight.py
@@ -90,11 +90,11 @@ class PVPFight:
         elif self.offender.hp <= 0:
             combat_log += f"\n{self.offender.name} has fallen."
             self.offender.gold += self.gold_amount_got(self.offender)
-            return combat_log, 2, self.defender, self.offender
+            return combat_log, 2
         elif self.defender.hp <= 0:
             combat_log += f"\n{self.defender.name} has fallen"
             self.offender.gold += self.gold_amount_got(self.offender)
-            return combat_log, 1, self.defender, self.offender
+            return combat_log, 1
         else:
             return combat_log, 0
 
